6df038e8a150bd0429339a08ac56309cd42e722a
Hannah Woodward <hannah@woodward.sh>
2023-10-01 15:48:37 +0100

    Fix missing lfs file retrieval

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,7 @@ FROM tiangolo/uvicorn-gunicorn:python3.8-alpine3.10
 
 RUN pip install --no-cache-dir starlette frontmatter jinja2 markdown
 
+# Install git lfs for pulling down lfs-stored resources during deployment
+RUN apk add --no-cache git-lfs
+
 COPY ./app /app
--- app/prestart.sh
+++ app/prestart.sh
@@ -1,1 +1,7 @@
 #! /usr/bin/env bash
+
+# Railway does not support git lfs out the box,
+# so re-clone repo here to retrieve resources stored in git lfs
+git clone https://github.com/hannahwoodward/woodward.sh.git tmp
+mv tmp/app/static/uploads static/uploads
+rm -rf tmp
