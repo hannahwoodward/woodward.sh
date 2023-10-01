98083ee2a2cfd2eb04c45d1da4d725dbe2c3b01d
Hannah Woodward <hannah@woodward.sh>
2023-10-01 16:36:53 +0100

    Ensure tmp removed at beginning of deploy

--- app/prestart.sh
+++ app/prestart.sh
@@ -2,7 +2,7 @@
 
 # Railway does not support git lfs out the box,
 # so download retrieve resources stored in git lfs
-rm -rf static/uploads
+rm -rf static/uploads tmp
 mkdir tmp
 cd tmp
 wget https://github.com/hannahwoodward/woodward.sh/archive/refs/heads/main.zip
