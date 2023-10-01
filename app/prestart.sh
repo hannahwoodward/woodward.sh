d9f0c00c00b9d5aa4083eca898ed85067e7cf313
Hannah Woodward <hannah@woodward.sh>
2023-10-01 16:15:35 +0100

    Remove old static/uploads dir

--- app/prestart.sh
+++ app/prestart.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/env bash
 
 # Railway does not support git lfs out the box,
-# so do manual download retrieve resources stored in git lfs
+# so download retrieve resources stored in git lfs
+rm -rf static/uploads
 mkdir tmp
 cd tmp
 wget https://github.com/hannahwoodward/woodward.sh/archive/refs/heads/main.zip
