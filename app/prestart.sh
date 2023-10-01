19bb3cc1bd1f25be42bb3189a52196eb5fc9eece
Hannah Woodward <hannah@woodward.sh>
2023-10-01 16:10:18 +0100

    Switch to repo zip download for lfs files

--- app/prestart.sh
+++ app/prestart.sh
@@ -1,10 +1,11 @@
 #! /usr/bin/env bash
 
 # Railway does not support git lfs out the box,
-# so re-clone repo here to retrieve resources stored in git lfs
-rm -rf tmp
-git lfs install
-git clone https://github.com/hannahwoodward/woodward.sh.git tmp
-cd tmp && git lfs fetch --all
-cd .. && mv tmp/app/static/uploads static/uploads
+# so do manual download retrieve resources stored in git lfs
+mkdir tmp
+cd tmp
+wget https://github.com/hannahwoodward/woodward.sh/archive/refs/heads/main.zip
+unzip main.zip
+cd ..
+mv tmp/*/app/static/uploads static/uploads
 rm -rf tmp
